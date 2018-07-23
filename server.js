7ed916ebd6e401ec3eeddef94aea6f83c032b7c0
Francisco <francisco.serrano372@gmail.com>
2018-07-23 17:09:20 -0300

    Cambios para hacer el deploy en Heroku

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const path = require('path');
 
 const app = express();
 
+const testFolder = './';
+const fs = require('fs');
+
+
+
 // Serve only the static files form the dist directory
 app.use(express.static('/dist/client-prototipo'));
 
 app.get('/*', function(req, res) {
     console.log('QUE ONDA BIGOTE')
     console.log(__dirname);
+    fs.readdirSync(testFolder).forEach(file => {
+        console.log(file);
+    });
     res.sendFile(path.join('/dist/client-prototipo/index.html'));
 });
 
