d1618ac583efcf55a9053e02abac7b2eec3379b8
Francisco <francisco.serrano372@gmail.com>
2018-08-07 13:13:47 -0300

    Corregido el título de la toolbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  textoTitulo = 'Ingeniería de Software - Grupo Bióticos';
 
   constructor() { }
 
